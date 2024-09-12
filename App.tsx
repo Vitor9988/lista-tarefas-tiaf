e345f8880145d5da04709c5671f4d0302b88abab
Vitor9988 <vtortavaresbs699@gmail.com>
2024-09-12 20:25:57 -0300

    Atualiza a configuração inicial do app com Native Base

--- App.tsx
+++ App.tsx
@@ -1,16 +1,23 @@
-import React from 'react';
-import { NativeBaseProvider, Box, Text, Center } from 'native-base';
+// Importa o provedor de estado global
+import { ProvedorEstadoGlobal } from "./src/hooks/EstadoGlobal";
+// Importa o componente ListaTarefas
+import ListaTarefas from "./src/components/ListaTarefas";
+import { NativeBaseProvider, View } from "native-base";
+import AdicionarTarefa from "./src/components/AdicionarTarefa";
 
 export default function App() {
+  // Retorna a estrutura da tela principal
   return (
+    // Envolve a aplicação no provedor de estado global
     <NativeBaseProvider>
-      <Center flex={1} bg="white">
-        <Box>
-          <Text fontSize="2xl" color="primary.500" fontWeight="bold" fontFamily="Roboto">
-            Olá TDSZ, nosso app está configurado com Native Base!
-          </Text>
-        </Box>
-      </Center>
+      <ProvedorEstadoGlobal>
+        <View style={{ flex: 1 }}>
+          {/* Componente para adicionar tarefas */}
+          <AdicionarTarefa />
+          {/* Componente que lista as tarefas */}
+          <ListaTarefas />
+        </View>
+      </ProvedorEstadoGlobal>
     </NativeBaseProvider>
   );
 }
